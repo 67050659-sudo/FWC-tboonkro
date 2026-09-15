33f1df1f984028408ea2044d1c271d73f451619c
Ryxeno <quaintgamer9@gmail.com>
2026-09-15 17:13:54 +0700

    Changes to be committed: 	deleted:    discovery_piscine/background.html 	deleted:    discovery_piscine/background.js 	renamed:    discovery_piscine/background.css -> discovery_piscine/cell03/ex00/background.css 	new file:   discovery_piscine/cell03/ex00/background.html 	new file:   discovery_piscine/cell03/ex00/background.js

---
+++ discovery_piscine/cell03/ex00/background.js
@@ -0,0 +1,8 @@
+const myButton = document.getElementById('Le_Button');
+const myBack = document.getElementById('Le_Back')
+
+myButton.addEventListener('click', function(){
+    
+    myBack.style.backgroundColor = 'Blue'
+
+});
