3ab31174b56858c4c4f4b6f1163851ad88c5cf8d
Ryxeno <quaintgamer9@gmail.com>
2026-09-16 10:33:44 +0700

    Changes to be committed: 	new file:   discovery_piscine/cell03/ex01/balloon.css 	new file:   discovery_piscine/cell03/ex01/balloon.html 	new file:   discovery_piscine/cell03/ex01/balloon.js 	new file:   discovery_piscine/cell03/ex02/calc.css 	new file:   discovery_piscine/cell03/ex02/calc.html 	new file:   discovery_piscine/cell03/ex02/calc.js

---
+++ discovery_piscine/cell03/ex01/balloon.js
@@ -0,0 +1,50 @@
+const myBalloon_Butt = document.getElementById('The_Balloon_Butt');
+const Balloon_Size = document.getElementById('Balloon_Color');
+
+let balloonsize_inc = 200;
+let color_range = 0;
+
+const balloon_Color = ['Red' , 'Green', 'Blue', 'Black']
+
+myBalloon_Butt.addEventListener('click', function(){
+
+    balloonsize_inc += 10;
+    color_range += 1;
+
+    if (color_range > 3){
+        color_range = 0;
+    }
+    
+    
+    if (balloonsize_inc > 420){
+        balloonsize_inc = 200;
+    }
+
+    Change_Balloon();
+    
+});
+
+Balloon_Size.addEventListener('mouseleave', function(){
+
+    balloonsize_inc -= 5;
+
+    if (balloonsize_inc < 200){
+        balloonsize_inc = 200;
+    }
+
+    color_range -= 1;
+    if (color_range < 0){
+        color_range = 3;
+    }    
+
+    Change_Balloon();
+
+})
+
+function Change_Balloon(){
+    Balloon_Size.style.height = balloonsize_inc + 'px';
+    Balloon_Size.style.width = balloonsize_inc + 'px';
+    myBalloon_Butt.style.height = balloonsize_inc + 'px';
+    myBalloon_Butt.style.width = balloonsize_inc + 'px';
+    Balloon_Size.style.backgroundColor = (balloon_Color[color_range]);
+}
